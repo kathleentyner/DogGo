05d859c42bfc9d25cd4da87392551054bc4a5d30
kathleentyner <125293552+kathleentyner@users.noreply.github.com>
2023-06-27 11:23:48 -0400

    chapters 1-4

---
+++ Models/Dog.cs
@@ -0,0 +1,17 @@
+﻿using DogGo.Models;
+using System.Net;
+using System.Security.Cryptography.Xml;
+
+namespace DogGo.Models
+{
+    public class Dog
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public string Notes { get; set; }
+        public string ImageUrl { get; set; }
+        public int OwnerId { get; set; }
+        public Owner Owner { get; set; }
+    }
+}
---
+++ Models/ViewModels/WalkerProfileViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace DogGo.Models.ViewModels
+{
+    public class WalkerProfileViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Walker Walker{ get; set; }
+        public List<Walk> Walks { get; set; }
+    }
+}
